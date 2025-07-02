c2aae641b33cfaa5f2777e84418d9e7c9fdb9ca6
kopacz77 <a_kopacz_77@hotmail.com>
2025-07-02 08:19:01 -0700

    z

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -24,7 +24,7 @@ const AdminLogin = ({ onLogin }) => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3001/api/admin/login', {
+      const response = await fetch('/api/admin/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(credentials)
